d83a78b0423cb0c4037b8c7ae9313d94b3ae9e4f
Radovan <69589463+CodeEdu2020@users.noreply.github.com>
2025-01-08 18:49:31 +0100

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,114 @@
+const cartItems = document.querySelector("#cart-items");
+const cartDelete = document.querySelector("#obrisi");
+const posaljiMejl = document.querySelector("#posaljiMejl");
+
+posaljiMejl.addEventListener("click", finalizeOrder);
+cartDelete.addEventListener("click", obrisi);
+
+function renderCart() {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  if (cart.length == 0) {
+    cartItems.textContent = "Nema nista u korpi!!!";
+    total();
+    cartDelete.style.display = "none";
+    return;
+  }
+
+  cartItems.innerHTML = "";
+
+  cart.forEach((item, index) => {
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+    div.innerHTML = `
+        <img src="${item.img}" alt="${item.title}" class="imgCart">
+        <h3>${item.title}</h3>
+        <p>Cena: ${item.price}</p>
+        <div>
+        <button class="minus">-</button>
+        ${item.quantity}
+        <button class="plus">+</button>
+        </div>
+        <button class="deleteItem">Ukloni</button>
+        `;
+    cartItems.appendChild(div);
+
+    div.querySelector(".plus").addEventListener("click", function () {
+      changeQuantity(index, 1);
+    });
+
+    div.querySelector(".minus").addEventListener("click", function () {
+      changeQuantity(index, -1);
+    });
+
+    div.querySelector(".deleteItem").addEventListener("click", function () {
+      deleteItem(index);
+    });
+  });
+  total();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  renderCart();
+});
+
+function changeQuantity(index, change) {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  if (cart[index].quantity + change > 0) {
+    cart[index].quantity += change;
+  }
+  //   } else {
+  //     // cart.splice(index, 1);
+  //   }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+  total();
+  renderCart();
+}
+
+function deleteItem(index) {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.splice(index, 1);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  total();
+  renderCart();
+}
+
+function total() {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  const total = cart.reduce((sum, item) => sum + item.quantity * item.price, 0);
+
+  document.querySelector("#total-price").textContent = total.toFixed(2);
+}
+
+function obrisi() {
+  localStorage.removeItem("cart");
+  renderCart();
+}
+
+function sendMail() {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const total = cart.reduce((sum, item) => sum + item.quantity * item.price, 0);
+
+  const template = {
+    to_email: "radovanbrajovic@gmail.com",
+    message: "Uspesna kupovina u iznosu oid " + total + " dinara",
+  };
+
+  emailjs.send("service_ml3qw7r", "template_sp9bt3w", template).then(
+    (response) => {
+      alert("Poslat je mejl");
+      obrisi();
+    },
+    (error) => {
+      console.log("FAILED...", error);
+    }
+  );
+}
+function finalizeOrder() {
+  if (confirm("Da li ste sigurni da zavrsavate kupovinu")) {
+    sendMail();
+  }
+}
---
+++ index.js
@@ -0,0 +1,166 @@
+let allProducts = [];
+
+const productList = document.querySelector(".product-list");
+const prev = document.querySelector("#prev-page");
+const next = document.querySelector("#next-page");
+const spinner = document.querySelector(".spinner");
+const pageInfo = document.querySelector("#page-info");
+const search = document.querySelector("#search");
+
+let currentPage = 1;
+const productPerPage = 20;
+let filtered;
+prev.addEventListener("click", function () {
+  changePage(-1);
+});
+
+next.addEventListener("click", function () {
+  changePage(1);
+});
+
+search.addEventListener("keyup", () => {
+  filterProducts();
+  renderProducts();
+  changePage(0);
+});
+
+const korpa = document.querySelector("#cart-link");
+korpa.addEventListener("drop", function (event) {
+  drop(event);
+});
+
+korpa.addEventListener("dragover", function (event) {
+  allowDrop(event);
+});
+
+function changePage(direction) {
+  let test = currentPage + direction;
+  const totalPG = Math.ceil(filtered.length / productPerPage); //preracunava se broj strana za datu pretragu
+
+  if (test == 0) {
+    test = 1;
+  } else if (test >= totalPG) {
+    test = totalPG;
+  }
+  next.style.display = "inline-block";
+  prev.style.display = "inline-block";
+
+  if (test == 1) {
+    prev.style.display = "none";
+  } else if (test == totalPG) {
+    next.style.display = "none";
+  }
+
+  currentPage = test;
+
+  pageInfo.textContent = `${currentPage} of ${totalPG}`; //uvek se menja ukupan broj strana u zavisnosti od searcha/da li smo samo ucitali stranu
+
+  renderProducts();
+}
+
+function fetchProducts() {
+  let xHr = new XMLHttpRequest();
+  spinner.style.display = "block";
+  xHr.addEventListener("readystatechange", function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let obj = JSON.parse(this.response);
+      allProducts = obj.products;
+      spinner.style.display = "none";
+      filterProducts(); //filtriraj proizvode odmah nakon ucitavanja
+      renderProducts();
+      changePage(0);
+    }
+  });
+
+  xHr.open("GET", "https://dummyjson.com/products?limit=100");
+  xHr.send();
+}
+
+
+function renderProducts() {
+  productList.innerHTML = "";
+
+  const startIndex = (currentPage - 1) * productPerPage;
+
+  const paginatedProducts = filtered.slice(
+    startIndex,
+    startIndex + productPerPage
+  );
+
+  paginatedProducts.forEach((elem) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    productDiv.setAttribute("draggable", true);
+    productDiv.addEventListener("dragstart", function (e) {
+      drag(e, elem);
+    });
+    productDiv.innerHTML = `
+       <img src="${elem.thumbnail}" alt="${elem.title}">
+       <h3>${elem.title}</h3>
+       <p>${elem.price}</p>
+       <button class="btnDodaj">Dodaj u korpu</button>
+       <button>❤</button>
+        
+        `;
+
+    console.log(elem);
+    productList.appendChild(productDiv);
+    productDiv
+      .querySelector(".btnDodaj")
+      .addEventListener("click", function () {
+        addToCart(elem);
+      });
+  });
+}
+
+function addToCart(data) {
+  let id = data.id;
+  let img = data.thumbnail;
+  let title = data.title;
+  let price = data.price;
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const existingItem = cart.find((elem) => elem.id === id);
+
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.push({ id, title, price, img, quantity: 1 });
+  }
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCount();
+  alert("Prozivod je dodat u korpu");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  updateCount();
+  fetchProducts();
+});
+
+function filterProducts() {
+  const searchV = search.value.toLowerCase();
+
+  filtered = allProducts.filter((product) => {
+    return product.title.toLowerCase().includes(searchV);
+  });
+}
+
+function updateCount() {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+  document.querySelector("#cart-count").textContent = totalItems;
+}
+
+function drag(event, product) {
+  const productData = JSON.stringify(product);
+  event.dataTransfer.setData("product", productData);
+}
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+function drop(e) {
+  e.preventDefault();
+  let product = e.dataTransfer.getData("product");
+  addToCart(JSON.parse(product));
+}
